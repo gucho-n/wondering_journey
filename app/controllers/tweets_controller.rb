5b72b2e4c962ae0d79f9ab6258779b5726d49582
gucho-n <i.m.your.dog.1192@gmail.com>
2020-10-10 19:39:01 +0900

    ユーザー登録、編集

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,15 +2,8 @@ class TweetsController < ApplicationController
   def index
     
   end
-  def new
-    
-  end
 
-  def create
-    @user = Tweet.new(stpara)
-    @user.save
-  end
-  def stpara 
-    params.require(:tweet)
-  end
+
+
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'tweets/index'
+  
   devise_for :users
-<<<<<<< HEAD
-  root to: 'tweets#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-=======
   resources :tweets
   root to: 'tweets#index'
->>>>>>> parent of d530501... Revert "最終修正２"
+
 end
