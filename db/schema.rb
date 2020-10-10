d530501e7813ba800d02a20e136b2041235ee5fd
gucho-n <i.m.your.dog.1192@gmail.com>
2020-10-10 13:32:12 +0900

    Revert "最終修正２"
    This reverts commit 8a44cce8863c84aaa87c5cef58a84efb98acbbb3.

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_103419) do
-
-  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "name", default: "", null: false
-    t.string "address", default: "", null: false
-    t.string "recommend", default: "", null: false
-    t.integer "cost"
-    t.integer "user_id"
-  end
+ActiveRecord::Schema.define(version: 2020_10_09_073523) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
