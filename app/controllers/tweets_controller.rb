18ba54dbff804ba15ebd34881dc5e8a6b06a1718
gucho-n <i.m.your.dog.1192@gmail.com>
2020-10-14 13:08:14 +0900

    ツイートのリンク

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,9 +1,21 @@
 class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
+    
   end
-  
-  
 
- 
+  def show
+   
+  end
+
+  def new
+   @tweet = Tweet.new
+  end
+
+  def create
+  @tweet = Tweet.create(tweet_params)
+  end
+  def tweet_params
+    params.require(:tweet).permit(:name, :address, :image, :recommend, :cost)
+  end
 end
