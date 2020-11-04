b2c0d8b2b3c3459c53fa6d1b00d8208bff49bc62
gucho-n <i.m.your.dog.1192@gmail.com>
2020-11-04 18:29:50 +0900

    投稿画面実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,21 +9,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname,:gender_id,:age,:surname,:firstname,:surname_kana,:firstname_kana,presence: true
+  validates :nickname,:gender_id,:age,presence: true
   
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
   
-  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
-  validates :surname, format: { with: VALID_NAME_REGEX }
-  validates :firstname, format: { with: VALID_NAME_REGEX }
+  # VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+  # validates :surname, format: { with: VALID_NAME_REGEX }
+  # validates :firstname, format: { with: VALID_NAME_REGEX }
 
-  # 氏名用かな
-  VALID_KANA_NAME_REGEX = /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/.freeze
-  validates :surname_kana, format: { with: VALID_KANA_NAME_REGEX }
-  validates :firstname_kana, format: { with: VALID_KANA_NAME_REGEX }
+  # # 氏名用かな
+  # VALID_KANA_NAME_REGEX = /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/.freeze
+  # validates :surname_kana, format: { with: VALID_KANA_NAME_REGEX }
+  # validates :firstname_kana, format: { with: VALID_KANA_NAME_REGEX }
 
-  # 氏名用かな
-    VALID_AGE_REGEX = /\A[0-9]+\z/.freeze
-    validates :age, format: { with: VALID_AGE_REGEX }
+  # # 氏名用かな
+  #   VALID_AGE_REGEX = /\A[0-9]+\z/.freeze
+  #   validates :age, format: { with: VALID_AGE_REGEX }
 end
