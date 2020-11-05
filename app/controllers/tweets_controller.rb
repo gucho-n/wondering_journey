461c8f747c07a2db4949d1cd3c1af8eaadb7ee9e
gucho-n <i.m.your.dog.1192@gmail.com>
2020-11-05 18:52:51 +0900

    googlemap整理

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,26 +1,22 @@
 class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
-  
-   
   end
 
   def show
    @tweet = Tweet.find(params[:id])
   #  ここではurlにあるパラムスを取得することによって指定したカラムを手にすることができます
    @comments = Comment.all
+
   end
-  
 
   def new
-
    @tweet = Tweet.new
   end
   #form_withは遷移先でインスタンス変数を生成すべきなのか？
 
   def create
    @tweet = Tweet.create(tweet_params)
-  
   end
   
   def tweet_params
