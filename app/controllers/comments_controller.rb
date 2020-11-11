84283bf21a925d85de894ac0f065dd2481aaddae
gucho-n <i.m.your.dog.1192@gmail.com>
2020-11-11 23:07:06 +0900

    条件分岐

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  
+  before_action :resist
   # コントローラーを新たに作成した理由はコメントの保存が必要だから
   # コメントのインスタンスの生成は、tweetcontrollerでもいいのかな？
 
@@ -14,5 +14,10 @@ end
   def comment_params
     # 左側のパラメーターはform_withで入力した値。後のmergeは自分で入力する値。（ params[:tweet_id]は遷移した時にとるtweetidを選んでいる）
     params.require(:comment).permit(:text).merge(user_id: current_user.id,tweet_id: params[:tweet_id])
+  end
+  def resist
+    unless user_signed_in?
+      redirect_to new_user_registration_path
+    end
   end
 end
