c2021c72e29f37ce1e6debdb39c4ccd10b351298
gucho-n <i.m.your.dog.1192@gmail.com>
2020-11-11 22:53:48 +0900

    javascript実装

--- app/javascript/list.js
+++ app/javascript/list.js
@@ -1,1 +1,14 @@
-document.getElementById("aaa")
+for (let i = 0; i < 6; i++){
+  window.addEventListener('load',function(){
+  const pullDownButton = document.getElementsByClassName("message-image")
+  const pullDownParents = document.getElementsByClassName("hover-lists")
+
+    pullDownButton[i].addEventListener('mouseover', function(){
+      pullDownParents[i].setAttribute("style", "display:block;")
+    })
+  pullDownButton[i].addEventListener('mouseout', function(){
+    pullDownParents[i].removeAttribute("style", "display:none;")
+  
+    })
+  })
+  }
